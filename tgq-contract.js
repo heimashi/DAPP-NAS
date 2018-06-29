f567af1516422a8ac07a2e9364ac4a687c16e992
shiwang <shiwang@rong360.com>
2018-06-29 16:38:34 +0800

    update

--- tgq-contract.js
+++ tgq-contract.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var TGQContract = function() {
+let TGQContract = function() {
     //tqg nas poll
     LocalContractStorage.defineProperty(this, "nasPerQ");  
     LocalContractStorage.defineProperty(this, "tgqPool");
@@ -15,34 +15,34 @@ var TGQContract = function() {
 TGQContract.prototype = {
 
     init: function() {
-        this.nasPerQ = 0.0001;
+        this.nasPerQ = 0.01;
         this.tgqPool = 0;
         this.bonusPool = 0;
 
         this.nasPerFree = 0.0005;
-        this.intervalFree = 60;
+        this.intervalFree = 120;
     },
 
     tgq: function(point) {
-        var from = Blockchain.transaction.from;
-        var value = Blockchain.transaction.value;
+        let from = Blockchain.transaction.from;
+        let value = Blockchain.transaction.value;
 
         if (value != this.nasPerQ * 1000000000000000000) {
             throw new Error("Please send " + this.nasPerQ + " NAS only");
         }
-        var inputPoint = point || 1;
+        let inputPoint = point || 1;
 
-        var currentPoint = Math.floor(Math.random() * 1000 + inputPoint);
+        let currentPoint = Math.floor(Math.random() * 1000 + inputPoint);
         
         this.hash_to_point.put(Blockchain.transaction.hash, currentPoint);
 
         this.tgqPool = this.tgqPool + this.nasPerQ * 0.8;
         this.bonusPool = this.bonusPool + this.nasPerQ * 0.2;
-        var awardAmount;
+        let awardAmount;
         if (currentPoint%1000===0) {
-            var tmp = this.tgqPool > this.nasPerQ * 50? (this.tgqPool - this.nasPerQ * 50): this.tgqPool ;
+            let tmp = this.tgqPool > this.nasPerQ * 50? (this.tgqPool - this.nasPerQ * 50): this.tgqPool ;
             awardAmount = new BigNumber(tmp + this.bonusPool);
-            var result = Blockchain.transfer(from, awardAmount * 1000000000000000000);
+            let result = Blockchain.transfer(from, awardAmount * 1000000000000000000);
             console.log("transfer result", result);
             if (!result) {
                 throw new Error("transfer failed.");
@@ -57,10 +57,10 @@ TGQContract.prototype = {
             this.tgqPool = this.tgqPool - tmp;
             this.bonusPool = 0;
         } else if (currentPoint%100===0) {
-            var tmp = this.tgqPool > this.nasPerQ * 20? (this.tgqPool - this.nasPerQ * 20): this.tgqPool ;
+            let tmp = this.tgqPool > this.nasPerQ * 20? (this.tgqPool - this.nasPerQ * 20): this.tgqPool ;
             awardAmount = new BigNumber(tmp + this.bonusPool);
             this.bonusPool = 0;
-            var result = Blockchain.transfer(from, awardAmount * 1000000000000000000);
+            let result = Blockchain.transfer(from, awardAmount * 1000000000000000000);
             console.log("transfer result", result);
             if (!result) {
                 throw new Error("transfer failed.");
@@ -76,9 +76,9 @@ TGQContract.prototype = {
             this.tgqPool = this.tgqPool - tmp;
             this.bonusPool = this.bonusPool - this.bonusPool*0.2;
         } else if (currentPoint%10===0) {
-            var tmp = this.tgqPool > this.nasPerQ * 5? (this.tgqPool - this.nasPerQ * 5): this.tgqPool ;
+            let tmp = this.tgqPool > this.nasPerQ * 5? (this.tgqPool - this.nasPerQ * 5): this.tgqPool ;
             awardAmount = new BigNumber(tmp + this.bonusPool);
-            var result = Blockchain.transfer(from, awardAmount * 1000000000000000000);
+            let result = Blockchain.transfer(from, awardAmount * 1000000000000000000);
             console.log("transfer result", result);
             if (!result) {
                 throw new Error("transfer failed.");
@@ -110,18 +110,18 @@ TGQContract.prototype = {
     },
 
     donateTgq: function() {
-        var from = Blockchain.transaction.from;
-        var value = Blockchain.transaction.value;
+        let from = Blockchain.transaction.from;
+        let value = Blockchain.transaction.value;
 
         this.tgqPool = this.tgqPool + value / 1000000000000000000;
     },
 
     claimFree: function() {
-        var from = Blockchain.transaction.from;
-        var claimAmount = new BigNumber(this.nasPerFree);
+        let from = Blockchain.transaction.from;
+        let claimAmount = new BigNumber(this.nasPerFree);
 
-        var now = new Date().getTime();
-        var latest_time = this.userFree.get(from);
+        let now = new Date().getTime();
+        let latest_time = this.userFree.get(from);
 
         if (this.tgqPool < this.nasPerFree) {
             throw new Error("Don't have enough free NAS to claim!");
@@ -134,7 +134,7 @@ TGQContract.prototype = {
         }
         this.userFree.put(from, now);
 
-        var result = Blockchain.transfer(from, claimAmount * 1000000000000000000);
+        let result = Blockchain.transfer(from, claimAmount * 1000000000000000000);
         console.log("transfer result", result);
         if (!result) {
             throw new Error("transfer failed.");
